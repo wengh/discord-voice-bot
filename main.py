05485ea7750c4912ae659b78e1333176e87a6ac0
wengh <wenghy02@gmail.com>
2025-05-09 23:15:56 -0400

    hello world

--- main.py
+++ main.py
@@ -1,20 +1,34 @@
-import discord
+import os
+from typing import Optional
 
-intents = discord.Intents.default()
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()  # take environment variables
+
+
+intents: discord.Intents = discord.Intents.default()
 intents.message_content = True
 
-client = discord.Client(intents=intents)
+client: discord.Client = discord.Client(intents=intents)
+
 
 @client.event
-async def on_ready():
-    print(f'We have logged in as {client.user}')
+async def on_ready() -> None:
+    print(f"We have logged in as {client.user}")
+
 
 @client.event
-async def on_message(message):
+async def on_message(message: discord.Message) -> None:
     if message.author == client.user:
         return
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+    if message.content.startswith("$hello"):
+        await message.channel.send("Hello!")
 
-client.run('your token here')
+
+token: Optional[str] = os.getenv("BOT_TOKEN")
+if token is not None:
+    client.run(token)
+else:
+    print("Error: BOT_TOKEN not found in environment variables.")
